d2c8a2291053c73c297b014fe7a4620e5eb0338f
Shane Geary <stg4462@comcast.net>
2020-01-29 19:47:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  h
+  holiday_hash.each do ||
 end
 
 def all_holidays_with_bbq(holiday_hash)
